1a9ea7fb23d61e20d8f416f53ad75523dfbfcc6a
Ксения Будкина <obscure.74@yandex.ru>
2026-01-17 04:04:30 +0300

    Исправление редактирования и удаления комментариев

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
     path(
         'category/<slug:category_slug>/',
-        views.category_postадфs,
+        views.category_posts,
         name='category_posts'
     ),
 
